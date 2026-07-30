a378a961c1e96ae78481c350f7100a2ed934d438
Agoufack Alapani Corantin Junior <corantin2005@gmail.com>
2026-07-30 22:34:35 +0100

    chore: remove unused techLabels after Built on badges removal

--- src/modules/Landing/constants/landing.constants.ts
+++ src/modules/Landing/constants/landing.constants.ts
@@ -66,11 +66,3 @@ export const scoreWeightPcts: number[] = [30, 25, 25, 10, 10];
 export const tierRanges: string[] = ["0 to 39", "40 to 69", "70 to 84", "85 to 100"];
 
 export const statValues: string[] = ["~15%", "~43%", "~50%", "<5%"];
-
-export const techLabels: string[] = [
-  "React",
-  "Laravel",
-  "MySQL",
-  "REST APIs",
-  "Sanctum auth",
-];
